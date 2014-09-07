07f13c884fb7c51469dbe4b7ce7a4fb45a60a92e
ervitis <vitomarti@gmail.com>
2014-09-07 19:15:34 +0200

    Tests

--- hatter/tests/test_functions.py
+++ hatter/tests/test_functions.py
@@ -4,6 +4,29 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 
 from hatter.functions.validators import validate_codigo_postal, validate_dni
+from hatter.functions import horario
+
+import datetime
+
+fmt_wo_utc = '%Y-%m-%d %H:%M:%S'
+
+
+def create_utc_object(value):
+    dict_utc = {
+        'utc': value
+    }
+
+    return dict_utc
+
+
+def create_time_object(value):
+    dict_time = {
+        'horas':    value[2:3],
+        'minutos':  value[4:],
+        'signo':    value[:1],
+    }
+
+    return dict_time
 
 
 class ValidatorsTestCase(TestCase):
@@ -26,3 +49,28 @@ class ValidatorsTestCase(TestCase):
         self.assertRaises(ValidationError, validate_dni, value2)
         self.assertRaises(ValidationError, validate_dni, value3)
         self.assertRaises(ValidationError, validate_dni, value4)
+
+
+class FunctionsTestCase(TestCase):
+    """
+    Functions test case
+    """
+    def test_get_horas(self):
+        value_utc = create_utc_object('+0200')
+
+        r = horario.get_horas_minutos_utc(value_utc)
+
+        self.assertTrue(isinstance(r, dict))
+        self.assertEqual(r['horas'], 2)
+        self.assertEqual(r['minutos'], 0)
+
+    def test_time(self):
+        value_spain = horario.spain_timezone()
+        self.assertTrue(isinstance(value_spain, datetime.datetime))
+
+    def test_utc_from_dict(self):
+        dict_time = create_time_object('+0200')
+
+        r = horario.set_utc_from_dict(dict_time)
+        self.assertTrue(isinstance(r, str))
+        self.assertEqual(len(r), 6)
